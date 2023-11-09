e929c9549bbdfeba13baf23f861d0adb8d69d651
nick.banks <nick.banks@hyperjar.com>
2023-11-09 15:54:24 +0000

    Apply fix
    This is a fix for the many "unbalanced appearance transitions" logs

--- Sources/SnapshotTesting/Common/View.swift
+++ Sources/SnapshotTesting/Common/View.swift
@@ -1020,7 +1020,7 @@
     ) -> () -> Void {
       let rootViewController: UIViewController
       if viewController != window.rootViewController {
-        rootViewController = UIViewController()
+        rootViewController = RootViewController()
         rootViewController.view.backgroundColor = .clear
         rootViewController.view.frame = window.frame
         rootViewController.view.translatesAutoresizingMaskIntoConstraints =
@@ -1050,8 +1050,8 @@
 
       window.rootViewController = rootViewController
 
-      rootViewController.beginAppearanceTransition(true, animated: false)
-      rootViewController.endAppearanceTransition()
+      viewController.beginAppearanceTransition(true, animated: false)
+      viewController.endAppearanceTransition()
 
       rootViewController.view.setNeedsLayout()
       rootViewController.view.layoutIfNeeded()
@@ -1060,12 +1060,12 @@
       viewController.view.layoutIfNeeded()
 
       return {
-        rootViewController.beginAppearanceTransition(false, animated: false)
+        viewController.beginAppearanceTransition(false, animated: false)
         viewController.willMove(toParent: nil)
         viewController.view.removeFromSuperview()
         viewController.removeFromParent()
         viewController.didMove(toParent: nil)
-        rootViewController.endAppearanceTransition()
+        viewController.endAppearanceTransition()
         window.rootViewController = nil
       }
     }
@@ -1118,6 +1118,10 @@
         #endif
         return self.config.safeArea
       }
+    }
+
+    private final class RootViewController: UIViewController {
+        override var shouldAutomaticallyForwardAppearanceMethods: Bool { false }
     }
   #endif
 
